9ec62724bcbac42d851d28d5e86db8117a5b12c4
5laughter <5laughter.luna@gmail.com>
2019-04-07 22:34:12 +0900

    GetGoogleTrend module

--- googleTrend.py
+++ googleTrend.py
@@ -2,8 +2,15 @@
 from pytrends.request import TrendReq
 
 
-def googleTrend(a, b):
+def googleTrend(a):
     pytrend = TrendReq()
-    pytrend.build_payload(kw_list=[a,b], timeframe='now 7-d')
+    pytrend.build_payload(kw_list=[a], timeframe='now 7-d')
     interest_over_time_df = pytrend.interest_over_time()
-    print(interest_over_time_df.head())
+    semiResult = interest_over_time_df.head()
+    #print(semiResult.ix[1][0]) #ix[0~4][0~1]
+    sumOfChoice = 0
+    for i in range(0,5):
+        sumOfChoice += semiResult.ix[i][0]
+    print(sumOfChoice)
+    #print(semiResult)
+    return sumOfChoice
