a484387cb40b61d7efe9d7ac6c79ca3e2772a01d
5laughter <5laughter.luna@gmail.com>
2019-04-12 10:52:20 +0900

    Decide randomly and Show result

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request
 from instaCrawl import *
 from googleTrend import *
+from choose import *
+import random
 
 app = Flask(__name__)
 
@@ -11,13 +13,19 @@ def test():
 
 @app.route('/post', methods=['POST'])
 def post():
-    #choice1 = request.form['choice1']
-    #choice2 = request.form['choice2']
+    choice1 = request.form['choice1']
+    choice2 = request.form['choice2']
     #result1 = instaCrawl(choice1)
     #result2 = instaCrawl(choice2)
     #result = result1 + result2
     #print(result)
-    return render_template('loading.html')
+    #choose(choice1, choice2)
+    r = round(random.random(),2)
+    if r > 0.5:
+        result=choice1
+    else:
+        result = choice2
+    return render_template('loading.html', decision = result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
